0e4da3e4b5ddbd1c09b55c12ef0abbc7e190f03e
Milad-Rakhsha-NV <167464435+Milad-Rakhsha-NV@users.noreply.github.com>
2026-07-22 08:59:45 -0700

    Apply suggestions from code review
    Co-authored-by: Andrew Kaufman <akaufman@nvidia.com>
    Signed-off-by: Milad-Rakhsha-NV <167464435+Milad-Rakhsha-NV@users.noreply.github.com>

--- tests/test_scene_solver_vbd.py
+++ tests/test_scene_solver_vbd.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: Copyright (c) 2025 The Newton Developers
+# SPDX-FileCopyrightText: Copyright (c) 2026 The Newton Developers
 # SPDX-License-Identifier: Apache-2.0
 
 import unittest
